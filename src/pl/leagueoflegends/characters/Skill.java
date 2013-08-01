13e177444ce3761adf6e92c6bfd8e07d1e02a508
regzand <regzand@gmail.com>
2013-08-02 01:59:44 +0200

    Utworzono klase abstrakcyjna Skill

---
+++ src/pl/leagueoflegends/characters/Skill.java
@@ -0,0 +1,11 @@
+package pl.leagueoflegends.characters;
+
+public abstract class Skill {
+	
+	public abstract String getName();
+	public abstract String getDescription();
+	public abstract int getCooldown();		// in secounds
+	public abstract boolean isActive();
+	public abstract int getLevel();
+	
+}
