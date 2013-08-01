ceae4e67333f6d567f13287bfec1f224e5506c56
regzand <regzand@gmail.com>
2013-08-02 01:36:06 +0200

    Dodano initializacje komend oraz ich uruchamianie

--- src/pl/leagueoflegends/LoLPlugin.java
+++ src/pl/leagueoflegends/LoLPlugin.java
@@ -2,9 +2,21 @@ package pl.leagueoflegends;
 
 import java.util.logging.Logger;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import pl.leagueoflegends.cmd.CmdLol;
+import pl.leagueoflegends.cmd.engine.RCommand;
+import pl.leagueoflegends.cmd.engine.RCommandArgsException;
+import pl.leagueoflegends.cmd.engine.RCommandException;
+import pl.leagueoflegends.cmd.engine.RCommandPermissionsException;
+import pl.leagueoflegends.cmd.engine.RCommandPlayerOfflineException;
+import pl.leagueoflegends.cmd.engine.RCommandPlayerOnlyException;
+
 public class LoLPlugin extends JavaPlugin{
+	
+	private RCommand lolCommand;
   
 	public void onEnable(){
 		
@@ -15,6 +27,9 @@ public class LoLPlugin extends JavaPlugin{
 		//languages
 		Lang.loadMessages(Config.getConfig("language"));
 		
+		//commands
+		lolCommand = new CmdLol();
+		
 		//Success message
 		logInfo("Poprawnie zaladowany!");
 		logInfo("by regzand & adrenator & Dzikoysk @ Bukkit.pl");
@@ -24,6 +39,30 @@ public class LoLPlugin extends JavaPlugin{
 		
 	}
 	
+	public boolean onCommand(CommandSender s, Command c, String label, String[] args){
+		
+		try {
+			return lolCommand.perform(s, label, args);
+			
+		} catch (RCommandArgsException e) {			//SYNTAX
+			s.sendMessage(Lang.getMessage("error.wrongSyntax", e.getSyntax()));
+			
+		} catch (RCommandPermissionsException e) {	//PERMISSIONS
+			s.sendMessage(Lang.getMessage("error.permission"));
+			
+		} catch (RCommandPlayerOfflineException e) {//PLAYER OFFLINE
+			s.sendMessage(Lang.getMessage("error.playerOffline", e.getPlayerName()));
+			
+		} catch (RCommandPlayerOnlyException e) {	//PLAYER ONLY
+			s.sendMessage(Lang.getMessage("error.playerOnly"));
+			
+		} catch (RCommandException e) {				//UNKNOWN
+			s.sendMessage(Lang.getMessage("error.unknown"));
+			
+		}
+		
+		return true;
+	}
 	
 	// STATIC //
 	private static Logger logger = Logger.getLogger("minecraft");
