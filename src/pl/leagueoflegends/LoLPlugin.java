9a7b436474a09d6d2d305304fee3c2dc23a18a32
regzand <regzand@gmail.com>
2013-08-02 01:11:10 +0200

    Dodano statyczny logger

--- src/pl/leagueoflegends/LoLPlugin.java
+++ src/pl/leagueoflegends/LoLPlugin.java
@@ -1,15 +1,31 @@
 package pl.leagueoflegends;
 
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LoLPlugin extends JavaPlugin{
   
 	public void onEnable(){
 		
+		
+		logInfo("Poprawnie zaladowany!");
+		logInfo("by regzand & adrenator & Dzikoysk @ Bukkit.pl");
 	}
 	
 	public void onDisable(){
 		
 	}
 	
+	
+	// STATIC //
+	private static Logger logger = Logger.getLogger("minecraft");
+	private static String prefix = "[LeagueOfLegends]";
+	
+	public static void logInfo(String msg){
+		logger.info(prefix+" "+msg);
+	}
+	public static void logWarning(String msg){
+		logger.warning(prefix+" "+msg);
+	}
 }
