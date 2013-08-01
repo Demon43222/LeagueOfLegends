697e5f1db6b4ce707062e24c424fb59585cd8451
Jan <regzand@gmail.com>
2013-08-01 23:58:59 +0200

    Create LoLPlugin.java

---
+++ src/pl/leagueoflegends/LoLPlugin.java
@@ -0,0 +1,15 @@
+package pl.leagueoflegends;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LoLPlugin extends JavaPlugin{
+  
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+	
+}
