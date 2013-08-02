a1c938d8809987e67c0ddb63a4972dec4fcf34e6
regzand <regzand@gmail.com>
2013-08-02 15:09:42 +0200

    Dodano championa -> Ashe

---
+++ src/pl/leagueoflegends/champion/AsheChampion.java
@@ -0,0 +1,17 @@
+package pl.leagueoflegends.champion;
+
+import org.bukkit.inventory.ItemStack;
+
+public class AsheChampion extends Champion{
+
+	public AsheChampion() {
+		super("ashe", CharacterType.Marksman);
+	}
+
+	@Override
+	public ItemStack getAttribute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
