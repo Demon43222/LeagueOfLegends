bf5248209c91d5b352502cc8638239eddb84bd54
regzand <regzand@gmail.com>
2013-08-02 17:06:40 +0200

    Zarejestrowano nowe komendy

--- src/pl/leagueoflegends/LoLPlugin.java
+++ src/pl/leagueoflegends/LoLPlugin.java
@@ -8,6 +8,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import pl.leagueoflegends.cmd.CmdLol;
 import pl.leagueoflegends.cmd.CmdLolAdmin;
+import pl.leagueoflegends.cmd.CmdLolAdminBuilding;
+import pl.leagueoflegends.cmd.CmdLolAdminStat;
+import pl.leagueoflegends.cmd.CmdLolAdminStatReset;
+import pl.leagueoflegends.cmd.CmdLolJoin;
+import pl.leagueoflegends.cmd.CmdLolLeave;
+import pl.leagueoflegends.cmd.CmdLolSpectate;
+import pl.leagueoflegends.cmd.CmdLolStat;
+import pl.leagueoflegends.cmd.CmdLolStatReset;
 import pl.leagueoflegends.cmd.engine.RCommand;
 import pl.leagueoflegends.cmd.engine.RCommandArgsException;
 import pl.leagueoflegends.cmd.engine.RCommandException;
@@ -31,8 +39,28 @@ public class LoLPlugin extends JavaPlugin{
 		Lang.loadMessages(Config.getConfig("language"));
 		
 		//commands
+		//lol
 		lolCommand = new CmdLol();
+		//lol join <arena>
+		lolCommand.addSubCommands(new CmdLolJoin());
+		//lol leave
+		lolCommand.addSubCommands(new CmdLolLeave());
+		//lol spectate <arena>
+		lolCommand.addSubCommands(new CmdLolSpectate());
+		//lol stat [nick]
+		RCommand lolStat = new CmdLolStat();
+			//lol stat reset
+			lolStat.addSubCommands(new CmdLolStatReset());
+		lolCommand.addSubCommands(lolStat);
+		//lol admin
 		RCommand lolAdmin = new CmdLolAdmin();
+			//lol admin stat <nick>
+			RCommand lolAdminStat = new CmdLolAdminStat();
+				//lol admin stat reset <nick>
+				lolAdminStat.addSubCommands(new CmdLolAdminStatReset());
+			lolAdmin.addSubCommands(lolAdminStat);
+			//lol admin building
+			lolAdmin.addSubCommands(new CmdLolAdminBuilding());
 		lolCommand.addSubCommands(lolAdmin);
 		
 		//Success message
