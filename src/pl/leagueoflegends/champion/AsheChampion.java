89709c1a5791096b50531199a5decf494e6472f9
regzand <regzand@gmail.com>
2013-08-02 15:13:25 +0200

    Dodano skille

--- src/pl/leagueoflegends/champion/AsheChampion.java
+++ src/pl/leagueoflegends/champion/AsheChampion.java
@@ -2,10 +2,20 @@ package pl.leagueoflegends.champion;
 
 import org.bukkit.inventory.ItemStack;
 
+import pl.leagueoflegends.skills.EnchantedCrystalArrowAbility;
+import pl.leagueoflegends.skills.FrostShotAbility;
+import pl.leagueoflegends.skills.HawkshotAbility;
+import pl.leagueoflegends.skills.VolleyAbility;
+
 public class AsheChampion extends Champion{
 
 	public AsheChampion() {
 		super("ashe", CharacterType.Marksman);
+		
+		addSkill(new FrostShotAbility());
+		addSkill(new VolleyAbility());
+		addSkill(new HawkshotAbility());
+		addSkill(new EnchantedCrystalArrowAbility());
 	}
 
 	@Override
