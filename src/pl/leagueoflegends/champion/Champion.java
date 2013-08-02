a4d3459b95036ff5f55d83ba1fe1ba1005828720
regzand <regzand@gmail.com>
2013-08-02 15:05:22 +0200

    System update

--- src/pl/leagueoflegends/champion/Champion.java
+++ src/pl/leagueoflegends/champion/Champion.java
@@ -5,28 +5,47 @@ import java.util.List;
 
 import org.bukkit.inventory.ItemStack;
 
+import pl.leagueoflegends.Config;
 import pl.leagueoflegends.skills.Ability;
 
 public abstract class Champion {
 	
+	private final String id;
+	private final CharacterType type;
 	private List<Ability> skills = new ArrayList<Ability>();
 	
+	public Champion(String id, CharacterType type){
+		this.id = id;
+		this.type = type;
+	}
+
+	public String getId(){
+		return id;
+	}
+	public CharacterType getType(){
+		return type;
+	}
 	public Ability[] getSkills(){
 		return (Ability[])skills.toArray();
 	}
+	public String getName(){
+		return Config.getConfig("champions").getString(id+".name", id);
+	}
+	public String getDescription(){
+		return Config.getConfig("champions").getString(id+".description", "");
+	}
+	
 	
 	
 	// ABSTRACT //
-	public abstract String getName();
-	public abstract String getDescription();
 	public abstract ItemStack getAttribute();
-	public abstract CharacterType getType();
 	
 	// PROTECTED //
 	protected void addSkill(Ability s){
 		skills.add(s);
 	}
 	
+	// ENUM //
 	enum CharacterType{
 		Tank,
 		Support;
