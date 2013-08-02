a7502e21ea46465be17dc9e874c1e479febb1e22
regzand <regzand@gmail.com>
2013-08-02 14:52:05 +0200

    Utworzono

---
+++ src/pl/leagueoflegends/skills/EnchantedCrystalArrowAbility.java
@@ -0,0 +1,28 @@
+package pl.leagueoflegends.skills;
+
+public class EnchantedCrystalArrowAbility extends Ability {
+
+	public EnchantedCrystalArrowAbility(String id, int startLvl, int maxLvl) {
+		super(id, startLvl, maxLvl);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getCooldown() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getManaCost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isActive() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
