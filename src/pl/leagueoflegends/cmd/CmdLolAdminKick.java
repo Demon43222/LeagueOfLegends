b19fbf4f0914227e67a7fbabb55e8ce31899383f
adrenator <adrenatorr@gmail.com>
2013-08-02 23:22:39 +0200

    Dodane komende

--- src/pl/leagueoflegends/cmd/CmdLolAdminKick.java
+++ src/pl/leagueoflegends/cmd/CmdLolAdminKick.java
@@ -1,1 +1,29 @@
-dfdf
+package pl.leagueoflegends.cmd;
+
+
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import pl.leagueoflegends.cmd.engine.RCommand;
+import pl.leagueoflegends.cmd.engine.RCommandException;
+
+public class CmdLolAdminKick extends RCommand {
+
+  public CmdLolAdminKick() {
+		super("kick");
+	}
+
+	@Override
+	public boolean execute(CommandSender s, String label, String[] args)
+			throws RCommandException {
+		
+		super.checkPermissions(s, "lol.admin.kick");
+		
+		Player p = super.checkPlayer(s);
+		
+		
+		return true;
+	}
+
+}
