6983f661ccd734db9c93a575de2747ea91910d81
adrenator <adrenatorr@gmail.com>
2013-08-03 00:36:32 +0200

    dodano

--- src/pl/leagueoflegends/cmd/CmdLolAdminUnban.java
+++ src/pl/leagueoflegends/cmd/CmdLolAdminUnban.java
@@ -1,1 +1,23 @@
- mj
+package pl.leagueoflegends.cmd;
+
+import org.bukkit.command.CommandSender;
+
+import pl.leagueoflegends.cmd.engine.RCommand;
+import pl.leagueoflegends.cmd.engine.RCommandException;
+
+public class CmdLolAdminUnban extends RCommand{
+
+  
+	public CmdLolAdminUnban(){
+		super("unban");
+	}
+
+	@Override
+	public boolean execute(CommandSender s, String label, String[] args)
+			throws RCommandException {
+	super.checkPermissions(s, "lol.admin.unban");
+	super.checkArguments(args, 1, true, "/"+label+" <nick>");
+  //TODO ub
+		return true;
+	}
+}
