85a65e9ee0ed7ab185e4f69715095e0cc37f9d5a
adrenator <adrenatorr@gmail.com>
2013-08-03 16:06:25 +0200

    Dodano cmd

---
+++ src/pl/leagueoflegends/cmd/CmdLoLChampions.java
@@ -0,0 +1,22 @@
+package pl.leagueoflegends.cmd;
+
+import org.bukkit.command.CommandSender;
+
+import pl.leagueoflegends.cmd.engine.RCommand;
+import pl.leagueoflegends.cmd.engine.RCommandException;
+
+public class CmdLoLChampions extends RCommand {
+	
+	public CmdLoLChampions(){
+		super("champions");
+	}
+
+	@Override
+	public boolean execute(CommandSender s, String label, String[] args)
+			throws RCommandException {
+		super.checkPermissions(s, "lol.player.champions");
+		
+		return true;
+	}
+
+}
