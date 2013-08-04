2887d14653aec7b7fdf442b8d4b95a6ad1ae20b2
Dzikoysk <dzikoysk@interia.eu>
2013-08-04 21:49:08 +0200

    Create SatchelCharge.java

---
+++ src/pl/leagueoflegends/skills/SatchelCharge.java
@@ -0,0 +1,26 @@
+package pl.leagueoflegends.skills;
+
+public class SatchelCharge extends Ability {
+
+  private static final int[] cooldown = new int[]{26, 24, 22, 20, 18};
+	
+	public BouncingBomb() {
+		super("bouncingBomb", 0, 5);
+	}
+
+	@Override
+	public int getCooldown() {
+		return cooldown[getLevel()];
+	}
+
+	@Override
+	public int getManaCost() {
+		return 65;
+	}
+
+	@Override
+	public boolean isActive() {
+		return getLevel()!=0 && checkDelay(getLastUsetTime(), getCooldown());
+	}
+
+}
