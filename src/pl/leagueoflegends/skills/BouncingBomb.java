da96c1e1f89c1089e4470a267d179a1f703090dd
Dzikoysk <dzikoysk@interia.eu>
2013-08-04 21:37:01 +0200

    + BouncingBomb - Ziggs

---
+++ src/pl/leagueoflegends/skills/BouncingBomb.java
@@ -0,0 +1,26 @@
+package pl.leagueoflegends.skills;
+
+public class BouncingBomb extends Ability {
+
+	private static final int[] cooldown = new int[]{0, 6, 5, 4};
+	
+	public BouncingBomb() {
+		super("bouncingBomb", 0, 3);
+	}
+
+	@Override
+	public int getCooldown() {
+		return cooldown[getLevel()];
+	}
+
+	@Override
+	public int getManaCost() {
+		return 60;
+	}
+
+	@Override
+	public boolean isActive() {
+		return getLevel()!=0 && checkDelay(getLastUsetTime(), getCooldown());
+	}
+
+}
