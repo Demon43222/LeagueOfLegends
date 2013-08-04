14ae4e53e7e97ff4638dd2a9411b2c152bec95c3
Dzikoysk <dzikoysk@interia.eu>
2013-08-04 21:58:46 +0200

    Create MegaInfernoBomb.java

---
+++ src/pl/leagueoflegends/skills/MegaInfernoBomb.java
@@ -0,0 +1,26 @@
+package pl.leagueoflegends.skills;
+
+public class MegaInfernoBomb extends Ability {
+
+  private static final int[] cooldown = new int[]{0, 120, 105, 90};
+
+	public EnchantedCrystalArrowAbility() {
+		super("megaInfernoBomb", 0, 3);
+	}
+
+	@Override
+	public int getCooldown() {
+		return cooldown[getLevel()];
+	}
+
+	@Override
+	public int getManaCost() {
+		return 100;
+	}
+
+	@Override
+	public boolean isActive() {
+		return getLevel()!=0 && checkDelay(getLastUsetTime(), getCooldown());
+	}
+
+}
