ef182c6cc9ec5980f1f29a593f92f7c76ffdc76d
Dzikoysk <dzikoysk@interia.eu>
2013-08-04 22:45:22 +0200

    Create Tumble.java

---
+++ src/pl/leagueoflegends/skills/Tumble.java
@@ -0,0 +1,28 @@
+package pl.leagueoflegends.skills;
+
+public class Tumble extends Ability {
+
+	private static final int[] cooldown = new int[]{0, 6, 5, 4, 3, 2};
+
+	private static int[] mana = new int[] {50, 60, 70, 80, 90};
+
+	public BouncingBomb() {
+		super("bouncingBomb", 0, 5);
+	}
+
+	@Override
+	public int getCooldown() {
+		return cooldown[getLevel()];
+	}
+
+	@Override
+	public int getManaCost() {
+		return 30;
+	}
+
+	@Override
+	public boolean isActive() {
+		return getLevel()!=0 && checkDelay(getLastUsetTime(), getCooldown());
+	}
+
+}
