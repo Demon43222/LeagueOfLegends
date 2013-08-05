9a517fe615ea19b04278f705dcdc93e72eec41e4
Dzikoysk <dzikoysk@interia.eu>
2013-08-05 21:50:36 +0200

    Create BloodBoil.java

---
+++ src/pl/leagueoflegends/skills/BloodBoil.java
@@ -0,0 +1,24 @@
+package pl.leagueoflegends.skills;
+
+public class BloodBoil extends Ability {
+
+	public BloodBoil() {
+		super("bloodBoil", 0, 5);
+	}
+
+	@Override
+	public int getCooldown() {
+		return 15;
+	}
+
+	@Override
+	public int getManaCost() {
+		return 50;
+	}
+
+	@Override
+	public boolean isActive() {
+		return getLevel()!=0 && checkDelay(getLastUsetTime(), getCooldown());
+	}
+
+}
