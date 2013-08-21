5d7694ce43fb907c3dd30ff1c8208982e813cd54
Adrenator <adrenatorr@gmail.com>
2013-08-22 00:20:29 +0200

    Dodano kupowanie dla innego gracza.

--- src/pl/leagueoflegends/cmd/CmdLolShop.java
+++ src/pl/leagueoflegends/cmd/CmdLolShop.java
@@ -1,6 +1,7 @@
 package pl.leagueoflegends.cmd;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import pl.leagueoflegends.cmd.engine.RCommand;
 import pl.leagueoflegends.cmd.engine.RCommandException;
@@ -15,9 +16,15 @@ public class CmdLolShop extends RCommand{
 	public boolean execute(CommandSender s, String label, String[] args)
 			throws RCommandException {
 		super.checkPermissions(s, "lol.player.shop");
+		Player p = super.checkPlayer(s);
 		
+		String nick = p.getName().toLowerCase();
+         if(!(args == null)){
+        	 if(! (args.equals(nick))){
+        		 //TODO shop cmd - zakup dla innego gracza( taki prezent)
+        	 }
+         }
 		//TODO shop cmd
-		
 		
 		
 		return true;
