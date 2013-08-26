80867c48834a3f2375397d47fcd2aece166eddf2
Adrenator <adrenatorr@gmail.com>
2013-08-26 15:02:31 +0200

    Poprawiono permission

--- src/pl/leagueoflegends/cmd/CmdLoLTeleport.java
+++ src/pl/leagueoflegends/cmd/CmdLoLTeleport.java
@@ -16,9 +16,11 @@ public class CmdLoLTeleport extends RCommand {
 	public boolean execute(CommandSender s, String label, String[] args)
 			throws RCommandException {
 		super.checkArguments(args, 2, false, "/"+label+" <arena>");
-		super.checkPermissions(s, "lol.admin.teleport", "lol.admin.teleport."+args[0].toLowerCase());
 		Player p = super.checkPlayer(s);
-		//TODO teleportarenacmd
+		if(p.hasPermission("lol.admin.teleport") || p.hasPermission("lol.admin.teleport."+args[0].toLowerCase())){
+			
+		}
+		
 		return true;
 	}
 }
